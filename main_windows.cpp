7cd70a5a38280f1d1ffd879d26fc1e2602e76c77
mohamed6000 <mohamed_touiti@hotmail.com>
2025-12-12 20:38:20 +0100

    Functioning window

--- main_windows.cpp
+++ main_windows.cpp
@@ -1,7 +1,83 @@
 #include "general.h"
 
+#define UNICODE
+#define _UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+#define VC_EXTRALEAN
+#include <windows.h>
+
+#if COMPILER_CL
+#pragma comment(lib, "Gdi32.lib")
+#endif
+
+bool should_quit = false;
+
+static LRESULT CALLBACK win32_main_window_callback(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    switch (msg) {
+        case WM_CLOSE:
+        case WM_QUIT:
+            should_quit = true;
+            break;
+
+        default:
+            return DefWindowProcW(hwnd, msg, wparam, lparam);
+    }
+
+    return 0;
+}
+
 int main(void) {
-    print("Hello friend\n");
+    HINSTANCE hInstance = GetModuleHandleW(null);
+
+    WNDCLASSEXW wc = {};
+    wc.cbSize = size_of(WNDCLASSEXW);
+    wc.style                = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wc.cbClsExtra           = 0;
+    wc.cbWndExtra           = 0;
+    wc.hInstance            = hInstance;
+    wc.hCursor              = LoadCursorW(null, IDC_ARROW);
+    wc.hbrBackground        = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wc.lpszMenuName         = null;
+    wc.lpszClassName        = L"WindowClassName";
+
+    wc.lpfnWndProc          = win32_main_window_callback;
+
+
+    // Register the window class.
+    if (RegisterClassExW(&wc) == 0) {
+        write_string("RegisterClassExW returned 0.\n");
+        return 0;
+    }
+
+    HWND hwnd = CreateWindowExW(0,
+                                L"WindowClassName",
+                                L"OpenGL",
+                                WS_OVERLAPPEDWINDOW,
+                                CW_USEDEFAULT, CW_USEDEFAULT,
+                                CW_USEDEFAULT, CW_USEDEFAULT,
+                                null,
+                                null,
+                                (HINSTANCE)0,
+                                null);
+
+    if (hwnd == null) {
+        write_string("CreateWindowExW returned 0.\n");
+        return 0;
+    }
+
+    // Display the window.
+    UpdateWindow(hwnd);
+    ShowWindow(hwnd, SW_SHOW);
+
+    while (!should_quit) {
+        MSG msg;
+        while (PeekMessageW(&msg, null, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+
     return 0;
 }
 
