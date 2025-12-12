113178bc4813bfaf91a9e52f8181a1e5bc5852de
mohamed6000 <mohamed_touiti@hotmail.com>
2025-12-12 13:43:30 +0100

    Setting things up

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+
+cl /nologo /Z7 /Od -W4 -WX main_windows.cpp
---
+++ main_windows.cpp
@@ -0,0 +1,30 @@
+#include "general.h"
+
+int main(void) {
+    print("Hello friend\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define GENERAL_IMPLEMENTATION
+#include "general.h"
