8112f0c4a00f42f6c6c22fae965181c983b9a1a6
mohamed6000 <mohamed_touiti@hotmail.com>
2025-12-18 01:34:03 +0100

    Setting up OpenGL with X11

--- src/main_linux.cpp
+++ src/main_linux.cpp
@@ -2,6 +2,9 @@
 
 #include <X11/Xlib.h>
 
+#include <GL/glx.h>
+#include <GL/gl.h>
+
 bool should_quit = false;
 
 int main(void) {
@@ -11,10 +14,41 @@ int main(void) {
         return 0;
     }
 
-    int screen = XDefaultScreen(display);
-    u64 black_color = BlackPixel(display, screen);
-    u64 white_color = WhitePixel(display, screen);
+    // GLX version.
+    GLint majorGLX, minorGLX;
+    glXQueryVersion(display, &majorGLX, &minorGLX);
+    if ((majorGLX == 1) && (minorGLX < 2)) {
+        write_string("You need at least GLX 1.2.\n", true);
+        return 0;
+    }
 
+    print("GLX Version: %d.%d\n", majorGLX, minorGLX);
+
+    int screen_id   = XDefaultScreen(display);
+    u64 black_color = BlackPixel(display, screen_id);
+    u64 white_color = WhitePixel(display, screen_id);
+
+    // Choose visual.
+    GLint glx_attributes[] = {
+        GLX_RGBA,
+        GLX_DOUBLEBUFFER,
+        GLX_DEPTH_SIZE,    24,
+        GLX_STENCIL_SIZE,  8,
+        GLX_RED_SIZE,      8,
+        GLX_GREEN_SIZE,    8,
+        GLX_BLUE_SIZE,     8,
+        GLX_SAMPLE_BUFFERS,0,
+        GLX_SAMPLES,       0,
+        None
+    };
+
+    XVisualInfo *visual_info = glXChooseVisual(display, screen_id, glx_attributes);
+    if (!visual_info) {
+        write_string("Failed to glXChooseVisual.\n", true);
+        return 0;
+    }
+
+/*
     Window window = XCreateSimpleWindow(display,
                                         XDefaultRootWindow(display),
                                         0, 0,
@@ -22,10 +56,40 @@ int main(void) {
                                         0,
                                         white_color,
                                         black_color);
+*/
+
+    Window parent_window = RootWindow(display, screen_id);
+
+    Colormap colormap = XCreateColormap(display, parent_window, 
+                                        visual_info->visual, 
+                                        AllocNone);
+
+    XSetWindowAttributes window_attributes = {};
+    window_attributes.border_pixel      = black_color;
+    window_attributes.background_pixel  = white_color;
+    window_attributes.override_redirect = True;
+    window_attributes.colormap   = colormap;
+    window_attributes.event_mask = ExposureMask|KeyPressMask|StructureNotifyMask;
+
+    Window window = XCreateWindow(display, parent_window, 
+                                  0, 0,
+                                  800, 600,
+                                  0, 
+                                  visual_info->depth, 
+                                  InputOutput,
+                                  visual_info->visual,
+                                  CWBackPixel|CWColormap|CWBorderPixel|CWEventMask,
+                                  &window_attributes);
 
     const char *title = "Window Test (Linux)";
     Atom net_wm_name = XInternAtom(display, "_NET_WM_NAME", False);
-    Atom utf8_string = XInternAtom(display, "UTF8_STRING", False);
+    Atom utf8_string = XInternAtom(display, "UTF8_STRING",  False);
+
+/*
+    XWindowAttributes window_attributes = {};
+    XGetWindowAttributes(display, window, &window_attributes);
+    GC gc = XCreateGC(display, window, 0, null);
+*/
 
     XChangeProperty(display, window, net_wm_name, utf8_string, 
                     8, PropModeReplace, 
@@ -39,10 +103,30 @@ int main(void) {
         write_string("Failed to XSetWMProtocols.\n", true);
     }
 
-    Atom wm_protocols = XInternAtom(display, "WM_PROTOCOLS", False);
+    GLXContext gl_context = glXCreateContext(display, visual_info, null, True);
+    if (!gl_context) {
+        write_string("Failed to glXCreateContext.\n", true);
+        return 0;
+    }
 
-    XSelectInput(display, window, ExposureMask|KeyPressMask|StructureNotifyMask);
-    XMapWindow(display, window);
+    if (!glXMakeCurrent(display, window, gl_context)) {
+        write_string("Failed to glXMakeCurrent.\n", true);
+        return 0;
+    }
+
+    const GLubyte *gl_version  = glGetString(GL_VERSION);
+    const GLubyte *gl_vendor   = glGetString(GL_VENDOR);
+    const GLubyte *gl_renderer = glGetString(GL_RENDERER);
+    
+    print("OpenGL Version:  %s\n", gl_version);
+    print("OpenGL Vendor:   %s\n", gl_vendor);
+    print("OpenGL Renderer: %s\n", gl_renderer);
+
+    // XSelectInput(display, window, ExposureMask|KeyPressMask|StructureNotifyMask);
+    
+    XClearWindow(display, window);
+    XMapRaised(display, window);
+    // XMapWindow(display, window);
 
     // XSync(display, False);
 
@@ -54,8 +138,7 @@ int main(void) {
 
             switch (event.type) {
                 case ClientMessage:
-                    if (event.xclient.message_type == wm_protocols &&
-                        event.xclient.data.l[0] == wm_delete_window) {
+                    if ((Atom)event.xclient.data.l[0] == wm_delete_window) {
                         should_quit = true;
                     }
                     break;
@@ -65,6 +148,11 @@ int main(void) {
                     break;
 
                 case Expose:
+                    // Repaint.
+                    break;
+
+                case ConfigureNotify:
+                    // Handle resize here.
                     break;
 
                 case KeyPress:
@@ -73,13 +161,23 @@ int main(void) {
             }
         }
 
+        glClearColor(0.2f, 0.38f, 0.72f, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glXSwapBuffers(display, window);
     }
 
+    glXDestroyContext(display, gl_context);
+
+    XFree(visual_info);
+    XFreeColormap(display, colormap);
+    // XFreeGC(display, gc);
     XDestroyWindow(display, window);
     XCloseDisplay(display);
 
     return 0;
 }
+
 
 
 
